b5b91075832aec1a23c391dbe7563c253ac8e7ea
Joe Gawrieh <joe.gawrieh@protonmail.com>
2021-06-17 20:01:49 -0700

    Refactoring and adding additional methods

--- Python/_python/OOP/store_and_products.py
+++ Python/_python/OOP/store_and_products.py
@@ -1,7 +1,7 @@
 class Store:
-    def __init__(self, name, products_list=[]):
+    def __init__(self, name):
         self.store_name = name
-        self.products_list = products_list
+        self.products_list = []
 
     def add_product(self, new_product):
         self.new_product = new_product
@@ -9,34 +9,34 @@ class Store:
         return self
 
     def sell_product(self, id):
-        print(self.products_list[id])
+        removing = self.products_list[id]
         self.products_list.pop(id)
+        print(f"You just sold the product: {removing.product_name}!")
         return self
+
+    def inflation(self, percent_increase):
+        for item in range(len(self.products_list)):
+            self.products_list[item].update_price(percent_increase, True)
+    
+    def set_clearance(self, category, percent_discount):
+        for ctg in range(len(self.products_list)):
+            if self.products_list[ctg].category == category:
+                self.products_list[ctg].update_price(percent_discount, False)
 class Product:
     def __init__(self, name, price, category):
         self.product_name = name
         self.price = price
         self.category = category
-        self.product = [name, price, category]
 
     def update_price(self, percent_changed, is_increased):
         if is_increased == True:
-            self.price += self.price * percent_changed
+            self.price += self.price * (percent_changed / 100)
+            print(f"Looks like the {self.product_name} price increased, which is now ${self.price}")
         else:
-            self.price -= self.price / percent_changed
+            self.price -= self.price * (percent_changed / 100)
+            print(f"Looks like the {self.product_name} price just decreased! It's now ${self.price}")
         return self
 
     def print_info(self):
         print(f"Product Name: {self.product_name}; Product Category: {self.category}; Product Price: ${self.price}")
         return self
-
-store1 = Store("My First Store")
-product1 = Product("Bang", 3.50, "Energy Drinks")
-product2 = Product("Cliff Builder", 2.50, "Protein Bars")
-product3 = Product("Lifeblood", 15.99, "Multivitamins")
-
-store1.add_product(product1.product).add_product(product2.product).add_product(product3.product)
-
-print(store1.products_list)
-store1.sell_product(0)
-print(store1.products_list)
