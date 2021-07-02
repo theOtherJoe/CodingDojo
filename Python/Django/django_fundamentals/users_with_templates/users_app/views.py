1bd5961d06e4e4afc83966c16604aca5acdb7997
Joe Gawrieh <joegawrieh@joes-mbp.lan>
2021-07-01 20:14:34 -0700

    Pushing up users with templates

---
+++ Python/Django/django_fundamentals/users_with_templates/users_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from .models import User
+
+def index(request):
+    context = {
+        'users': User.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def process(request):
+    user1 = User.objects.create(
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        email = request.POST['email'],
+        age = request.POST['age']
+    )
+    return redirect('/')
